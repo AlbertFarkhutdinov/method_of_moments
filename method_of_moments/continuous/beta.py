a7f63d8ed4a7b87709c6cd8b6ac03e2eaa7e6f98
Albert Farhutdinov <farhutdinov.a@mxm.email>
2021-10-27 14:17:14 +0300

    added 'LogScale'

--- method_of_moments/continuous/beta.py
+++ method_of_moments/continuous/beta.py
@@ -12,58 +12,33 @@ from typing import Tuple
 
 from scipy.stats import beta
 
-from method_of_moments.continuous._base_continuous import BaseContinuous
+from ..loc_scale import LocScale
+from ._base_continuous import BaseContinuous
 
 
-class Beta(BaseContinuous):
-    """
-    Class for Beta distribution.
-
-    Parameters
-    ----------
-    max_bin : int
-        The value of `scale` parameter for scipy.stats.beta.
-    **kwargs : `base.BaseDistribution` properties.
-
-    """
-
-    def __init__(
-            self,
-            loc: float = 0.,
-            scale: float = 1.,
-            **kwargs,
-    ) -> None:
-        """Initialize self. See help(type(self)) for accurate signature."""
-        super().__init__(**kwargs)
-        self.loc = loc
-        self.scale = scale
-        _mean = (self.mean - loc) / scale
-        _variance = self.variance / scale ** 2
-        _factor = _mean * (1 - _mean) / _variance - 1.0
-        self.a_param = _factor * _mean
-        self.b_param = _factor * (1 - _mean)
+class Beta(BaseContinuous, LocScale):
+    """Class for Beta distribution."""
 
     @property
-    def scale(self) -> float:
-        """The value of `scale` parameter for scipy.stats.beta."""
-        return self.__scale
+    def a_parameter(self) -> float:
+        """Return parameter `A` in Beta distribution."""
+        _mean = self.get_standard_mean(self.mean)
+        _variance = self.get_standard_variance(self.variance)
+        return (_mean * (1 - _mean) / _variance - 1.0) * _mean
 
-    @scale.setter
-    def scale(
-            self,
-            scale: float = 1.,
-    ) -> None:
-        """Property setter for `self.scale`."""
-        if scale < 0:
-            raise ValueError('`scale` value must be positive.')
-        self.__scale = scale
+    @property
+    def b_parameter(self) -> float:
+        """Return parameter `B` in Beta distribution."""
+        _mean = self.get_standard_mean(self.mean)
+        _variance = self.get_standard_variance(self.variance)
+        return (_mean * (1 - _mean) / _variance - 1.0) * (1 - _mean)
 
     def pdf(self, arg: float) -> float:
         """Return probability density function at a given argument."""
         return beta.pdf(
             arg,
-            a=self.a_param,
-            b=self.b_param,
+            a=self.a_parameter,
+            b=self.b_parameter,
             loc=self.loc,
             scale=self.scale,
         )
@@ -72,12 +47,12 @@ class Beta(BaseContinuous):
         """Return cumulative density function at a given argument."""
         return beta.cdf(
             arg,
-            a=self.a_param,
-            b=self.b_param,
+            a=self.a_parameter,
+            b=self.b_parameter,
             loc=self.loc,
             scale=self.scale,
         )
 
     def get_parameters(self) -> Tuple[float, float]:
         """Return parameters of distribution."""
-        return self.a_param, self.b_param
+        return self.a_parameter, self.b_parameter
